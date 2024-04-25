1ff2d09a854f1d5c17e2dff8377ba4e40d4542a0
Zvbhrf <yamirka-su@mail.ru>
2024-04-25 22:25:51 +0500

    Temporary disable logging

--- lua/proptomia/sh_proptomia.lua
+++ lua/proptomia/sh_proptomia.lua
@@ -29,13 +29,13 @@ function proptomia.LogInfo(...)
     local s = concat(...)
 
     MsgC(color_wblue, "Proptomia ", color_gray, "[", color_blue, " INFO  ", color_gray, "]\t", color_white, s)
-    if signalLogger then signalLogger.Info("proptomia", s) end -- support for my logger :p
+    -- if signalLogger then signalLogger.Info("proptomia", s) end -- support for my logger :p
 end
 function proptomia.LogWarn(...)
     local s = concat(...)
 
     MsgC(color_wblue, "Proptomia ", color_gray, "[", color_yellow, " WARN  ", color_gray, "]\t", color_white, s)
-    if signalLogger then signalLogger.Warn("proptomia", s) end
+    -- if signalLogger then signalLogger.Warn("proptomia", s) end
 end
 function proptomia.LogError(...)
     local s = concat(...)
@@ -43,15 +43,15 @@ function proptomia.LogError(...)
     MsgC(color_wblue, "Proptomia ", color_gray, "[", color_red, " ERROR ", color_gray, "]\t", color_white, s)
     debug.Trace()
 
-    if signalLogger then signalLogger.Error("proptomia", s) end
+    -- if signalLogger then signalLogger.Error("proptomia", s) end
 end
 function proptomia.LogDebug(...)
-    local s = concat(...)
-
     if proptomia.debug then
+        local s = concat(...)
+
         MsgC(color_wblue, "Proptomia ", color_gray, "[", color_wblue, " DEBUG ", color_gray, "]\t", color_white, s)
     end
-    if signalLogger then signalLogger.Debug("proptomia", s) end
+    -- if signalLogger then signalLogger.Debug("proptomia", s) end
 end
 
 proptomia.LogInfo("Loading files...")
